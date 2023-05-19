d0bae22021dc447fecf8105e8df6e6ec1bbe523e
Juan Quino <quinojuan@gmail.com>
2023-05-19 09:07:03 -0300

    .

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -3,7 +3,7 @@ import whatsapp from "../assets/whatsapp.png";
 import "../components/WhatsAppButton.css";
 
 const WhatsAppButton = () => {
-  const phoneNumber = "1160300518"; // Agrega aquí tu número de teléfono
+  const phoneNumber = "5491160300518"; // Agrega aquí tu número de teléfono
 
   const handleWhatsAppClick = () => {
     const url = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
