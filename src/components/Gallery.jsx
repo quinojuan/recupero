c1b40cc7a8b4233d914d487772e7b7082eb69f2a
Juan Quino <quinojuan@gmail.com>
2023-05-19 08:38:37 -0300

    .

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import "../components/Gallery.css"
+import "../components/Gallery.css";
 
 const Gallery = ({ data }) => {
   return (
-    <div className="gallery">
-      {data.map((item) => (
-        <div key={item.id} className="card">
-          <img src={item.image} alt={item.description} />
-          <div className="details">
-            <p className="description">{item.description}</p>
-            <p className="price">${item.price}</p>
+    <>
+      <h2 className="title">Artículos a la venta</h2>
+      <div className="gallery">
+        {data.map((item) => (
+          <div key={item.id} className="card">
+            <img src={item.image} alt={item.description} />
+            <div className="details">
+              <p className="description">{item.description}</p>
+              <p className="price">${item.price}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
