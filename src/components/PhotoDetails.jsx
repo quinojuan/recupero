a02d66f9e5f9c6dd1b3acf359bfa4e4279f1d528
Juan Quino <quinojuan@gmail.com>
2023-05-21 06:43:25 -0300

    ajustando imagenes al centro en desktop

--- src/components/PhotoDetails.jsx
+++ src/components/PhotoDetails.jsx
@@ -9,26 +9,42 @@ export const PhotoDetails = ({ data }) => {
 
   console.log(newData);
   return (
-      <>
-      <h2 style={{
-          textAlign: "center"
-        }}>{newData[0].description}</h2>
+    <>
+      <h2
+        style={{
+          textAlign: "center",
+        }}
+      >
+        {newData[0].description}
+      </h2>
       {newData[0].additionalPhotos.length > 0 ? (
-          newData[0].additionalPhotos.map((photo, index) => (
-              <img
-              style={{ width: "100%" }}
+        newData[0].additionalPhotos.map((photo, index) => (
+          <div style={{
+            display: "flex",
+            justifyContent: "center",
+          }}>
+            <img
+              style={{
+                width: "60%",
+                margin: "10px auto"
+              }}
               key={index}
               src={photo}
-            alt={`Foto adicional ${index + 1}`}
+              alt={`Foto adicional ${index + 1}`}
             />
-            ))
-            ) : (
-                <h3 style={{
-                    color: "red",
-                    textAlign: "center"
-                }}>No hay fotos adicionales</h3>
-                )}
-<BackButton />
+          </div>
+        ))
+      ) : (
+        <h3
+          style={{
+            color: "red",
+            textAlign: "center",
+          }}
+        >
+          No hay fotos adicionales
+        </h3>
+      )}
+      <BackButton />
     </>
   );
 };
